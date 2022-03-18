299e2fa090604318a068f1f62e152b33b769ef3e
julin33 <418736745@qq.com>
2022-03-18 17:56:08 +0800

    test

---
+++ src/multithread/TestStop.java
@@ -0,0 +1,33 @@
+package multithread;
+
+/**
+ * 测试Stop
+ * 1. 建议线程正常停止（利用次数，不建议死循环）
+ * 2. 建议使用标志位（设置一个标志位）
+ * 3. 不要使用Stop或者Destroy等过时或者JDK不建议使用的方法
+ */
+public class TestStop implements Runnable{
+
+    //1. 设置一个标志位
+    private boolean flag = true;
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (flag) {
+            System.out.println("run....Thread" + i++);
+        }
+    }
+
+    //设置一个公开的方法停止线程，转换标志位
+    public void stop () {
+        this.flag = false;
+    }
+
+    public static void main(String[] args) {
+        TestStop testStop = new TestStop();
+
+        new Thread(testStop).start();
+
+    }
+}
