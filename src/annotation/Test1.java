50551728dd5561814796bd575927810bf6783de0
julin <418736745@qq.com>
2022-03-23 23:49:23 +0800

    11

--- src/annotation/Test1.java
+++ src/annotation/Test1.java
@@ -1,13 +1,28 @@
 package annotation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 什么是注解
+ * 内置注解
  */
 public class Test1 extends Object{
-
-    @Override //重写的注解
+    //@Override 重写的注解
+    @Override
     public String toString() {
         return super.toString();
+    }
+
+    // @Deprecated 不推荐程序员使用，或者存在更好的方式，但是可以使用
+    @Deprecated
+    public void test () {
+        System.out.println("Deprecated");
+    }
+
+    //SuppressWarnings 镇压警告
+    @SuppressWarnings("all")
+    public void test2 () {
+        List list = new ArrayList();
     }
 
 }
---
+++ src/annotation/Test2.java
@@ -0,0 +1,27 @@
+package annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 元注解:注解注解的注解
+ */
+public class Test2 {
+
+}
+
+//定义一个注解
+//Target 表示我们的注解可以用在哪些地方
+@Target(value = {ElementType.METHOD, ElementType.TYPE})
+
+//Retention 表示我们的注解在什么地方还有效
+// runtime > class >sources
+@Retention(value = RetentionPolicy.RUNTIME)
+
+//Documented 表示是否将我们的注解生成在JAVAdoc中
+@Documented
+
+//Inherited 子类可以继承父类的注解
+@Inherited
+@interface MyAnnotation{
+
+}
